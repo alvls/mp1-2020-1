0176a465a096b3421f766ba3665af32ef736b362
Громова Виктория <flapflapdany>
2020-03-02 21:28:09 +0300

    Исправила task1  и сделала русскую  кодировку в Task2

--- Gromova_va/task1/Source.cpp
+++ Gromova_va/task1/Source.cpp
@@ -22,6 +22,10 @@ public:
 		meters = lenn.meters;
 		return *this;
 	}
+	float met()
+	{
+		return meters;
+	}
 	float arshin()
 	{
 		return meters * 1.406074;
@@ -44,51 +48,69 @@ public:
 	}
 	void Printt()
 	{
-		cout « meters « " meters." « endl;
+		cout << meters << " meters." << endl;
 	}
 };
 int main(void)
 {
 	float a = -1;
-	cout « "Enter the length in meters: " « endl;
+	cout << "Enter the length in meters: " << endl;
 	do
 	{
-		cin » a;
+		cin >> a;
 		if (a < 0)
 		{
-			cout « "Enter the length in meters: " « endl;
+			cout << "Enter the length in meters: " << endl;
 		}
 	} while (a < 0);
 	lengths lenn(a);
-	int type = 0;
-	do
-	{
-		cout « "Enter the type of length measurement :\n 1) Arshin.\n 2)ft.\n 3)Inch.\n 4)Mile.\n 5)Sazhen." « endl;//ft-ôóò
-		cin » type;
-	} while ((type < 1) || (type > 5));
+	int type = 0, k = -1;
 	lengths check;
 	check = lenn;
+	cout << "Want to know the current length in meters?\n 1-yes,0-no " << endl;
+	cin >> k;
+	while ((k != 0) && (k != 1))
+	{
+		cout << ("Enter 1 or 0.\n");
+		cin >> k;
+	}
+	switch (k)
+	{
+	case 1:
+		check.Printt();
+		cout << " Current length  " << lenn.met() << " meters\n";
+		break;
+
+	case 0:
+
+		break;
+	}
+	do
+	{
+		cout << "Enter the type of length measurement :\n 1) Arshin.\n 2)ft.\n 3)Inch.\n 4)Mile.\n 5)Sazhen." << endl;//ft-ôóò
+		cin >> type;
+	} while ((type < 1) || (type > 5));
 	switch (type)
 	{
 	case 1:
 		check.Printt();
-		cout « " It's " « lenn.arshin() « " arshin\n";
+		cout << " It's " << lenn.arshin() << " arshin\n";
 		break;
 	case 2:
 		check.Printt();
-		cout « " It's " « lenn.ft() « " ft\n";
+		cout << " It's " << lenn.ft() << " ft\n";
 		break;
 	case 3:
 		check.Printt();
-		cout « " It's " « lenn.inch() « " inch\n";
+		cout << " It's " << lenn.inch() << "inch\n";
 		break;
 	case 4:
 		check.Printt();
-		cout « " It's " « lenn.mile() « " mile\n";
+		cout << " It's " << lenn.mile() << " mile\n";
 		break;
 	case 5:
 		check.Printt();
-		cout « " It's " « lenn.sazhen() « " sazhen\n";
+		cout << " It's " << lenn.sazhen() << " sazhen\n";
 		break;
 	}
 	system("pause");
