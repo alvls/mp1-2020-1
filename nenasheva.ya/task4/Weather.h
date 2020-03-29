df35b0013880b320bc3e263b08a80f2930640ae5
yanok5 <yana.nenasheva2001@gmail.com>
2020-03-29 18:59:07 +0300

    Task 4

---
+++ nenasheva.ya/task4/Weather.h
@@ -0,0 +1,33 @@
+#pragma once
+
+struct Observation
+{
+	int day, month, year;
+	int time;
+	int t;
+};
+
+class Weather
+{
+private:
+	Observation start;
+	Observation data[24][365];
+	int days[12] = { 31, 28, 31, 30, 31, 30,  31, 31, 30, 31, 30, 31 };
+	int Days_before(int);
+public:
+	Weather();
+	Weather(const Weather &);
+	Weather& operator=(const Weather &);
+	~Weather();
+	void Set_start(int, int, int, int);
+	Observation Get_start();
+	void Set_observation(int, int, int, int, int);
+	int Get_T(int, int, int, int);
+	void Save_to_file(char*);
+	void Read_from_file(char*);
+	void Set_series(int, int, int, int, int);
+	double Search_average_T_for_year();
+	double Search_average_T_for_month(int, int);
+	double Search_average_T_for_day(int, int, int);
+	double Search_average_T(int, int, int, int);
+};
