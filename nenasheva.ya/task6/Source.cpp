bb136e53d8759e9489019b3101d64df0d6f3dce2
yanok5 <yana.nenasheva2001@gmail.com>
2020-05-19 20:24:21 +0300

    Небольшие правки

--- nenasheva.ya/task6/Source.cpp
+++ nenasheva.ya/task6/Source.cpp
@@ -8,15 +8,19 @@ int main(void)
 	srand(time(NULL));
 	BullsAndCows BC;
 
+	cout << "Добро пожаловать в игру 'Быки и коровы'" << endl;
 	bool win = 0;
-	int length;
+	int n;
+	int num;
 	cout << "Введите длину числа: ";
-	cin >> length;
-	BC.Set_length(length);
+	cin >> n;
+	BC.Set_length(n);
 	BC.Set_number();
 	while (!win)
 	{
-		BC.Guessing();
+		cout << "Введите число из " << n << " знаков: ";
+		cin >> num;
+		BC.Guessing(num);
 		win = BC.Win_check();
 	}
 	system("pause");
