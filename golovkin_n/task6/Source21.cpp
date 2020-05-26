3bba624ea82f113bf0e256a2523faf80b196961d
GolovRibolov <kirgo12345@mail.ru>
2020-05-26 15:39:34 +0300

    oformil po crasotyshke

--- golovkin_n/task6/Source21.cpp
+++ golovkin_n/task6/Source21.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<cstdlib>
+#include<ctime>
 using namespace std;
 enum CellState { Empty, Miss, Deck, HitDeck, No};//—осто€ни€ клетки
 class Cell
@@ -50,9 +51,10 @@ class GameBoard
 {
 private:
 	static const int _size = 10;
-	Cell cells[_size][_size];
-	Cell cells1[_size][_size];
-	Cell cells2[_size][_size];
+	Cell cells[_size][_size];// поле игрока
+	Cell cells1[_size][_size];// поле кораблей бота
+	Cell cells2[_size][_size];// поле дл€ контрол€ выстрелов бота
+	Cell cells3[_size][_size];// поле бота высвечивающеес€ игроку
 	int sizeShip;
 	int Ship1 = 0;
 	int Ship2 = 0;
@@ -71,6 +73,9 @@ public:
 		for (int i = 0; i < _size; i++)
 			for (int j = 0; j < _size; j++)
 				cells1[i][j].SetState(Empty);
+		for (int i = 0; i < _size; i++)
+			for (int j = 0; j < _size; j++)
+				cells3[i][j].SetState(Empty);
 	}
 	void CreatorShipPlayer(int _sizeShip,int _y,int _x,bool horizontal)
 	{
@@ -99,16 +104,16 @@ public:
 	{
 		CellState p = cells1[_y][_x].GetState();
 		if (p == Empty)
-			cells1[_y][_x].SetState(Miss);
+			cells3[_y][_x].SetState(Miss);
 		else
-			cells1[_y][_x].SetState(HitDeck);
+			cells3[_y][_x].SetState(HitDeck);
 	}
 	bool GameEnd()
 	{
 		for (int i = 0; i < _size; i++)
 			for (int j = 0; j < _size; j++)
 			{
-				CellState p = cells[i][j].GetState();
+				CellState p = cells1[i][j].GetState();
 				if (p == Deck)
 					return false;
 			}
@@ -116,7 +121,7 @@ public:
 	}
 	void CreatorShipIi()
 	{
-		srand(NULL);
+		srand(time(NULL));
 		int x, y, sizeShip;
 		int gorizontal;
 		int count1 = 0;
@@ -132,7 +137,7 @@ public:
 				gorizontal = rand() % (1 - 0) + 0;
 				switch (gorizontal)
 				{
-				case 0://√оризонтальный 4-х палубник(vertik)
+				case 0://¬ертикальный 4-х палубник(vertik)
 					sizeShip = 4;
 					y = rand() % (6 - 0) + 0;
 					x = rand() % (9 - 0) + 0;
@@ -158,7 +163,7 @@ public:
 						x++;
 					}
 					break;
-				case 1: // ¬ертикальный 4-ч палубник
+				case 1: // √оризонтальный 4-ч палубник (Gorizont)
 					sizeShip = 4;
 					y = rand() % (9 - 0) + 0;
 					x = rand() % (6 - 0) + 0;
@@ -176,12 +181,11 @@ public:
 					{
 						if (x > 0)
 							if ((y > 0) && (y < 11))
-
-								cells1[y - i][x - 1].SetState(No);
+								cells1[y - 1][x - 1].SetState(No);
 						if (x < 6)
 							if ((y > 0) && (y < 11))
-
-								cells1[y - i][x + 4].SetState(No);
+								cells1[y - 1][x + 4].SetState(No);
+						y++;
 					}
 					break;
 				}
@@ -197,18 +201,21 @@ public:
 					gorizontal = rand() % (1 - 0) + 0;
 					switch (gorizontal)
 					{
-					case 0: // √оризонтальный(vertik)
+					case 0: // ¬ертикальный(vertik)
 						sizeShip = 3;
 						do
 						{
-							x = rand() % (7 - 0) + 0;
-							y = rand() % (9 - 0) + 0;
-						} while ((cells1[y][x].GetState() == Deck) || (cells1[y][x].GetState() == No));// проверка свободна ли клетка
-						for (int i = 0; i < 3;)// проверка на возможность установки корабл€
-							if (cells1[y + i][x].GetState() != No)
-								i++;
-							else
-								flag1 = false;
+							do
+							{
+								x = rand() % (9 - 0) + 0;
+								y = rand() % (7 - 0) + 0;
+							} while ((cells1[y][x].GetState() == Deck) || (cells1[y][x].GetState() == No));// проверка свободна ли клетка
+							for (int i = 0; i < 3;)// проверка на возможность установки корабл€
+								if (cells1[y + i][x].GetState() != No)
+									i++;
+								else
+									flag1 = false;
+						} while (flag1 == false);
 						if (flag1 == true)
 						{
 							for (int i = 0; i < sizeShip; i++)
@@ -225,33 +232,35 @@ public:
 							{
 								if (y > 0)
 									if ((x > 0) && (x < 11))
-
 										cells1[y - 1][x - 1].SetState(No);
 								if (y < 7)
 									if ((x > 0) && (x < 11))
-
 										cells1[y + 3][x - 1].SetState(No);
 								x++;
 							}
 							count++;
 						}
 						break;
-					case 1:// вертикальный
+					case 1:// √оризонтальный (Gorizont)
 						sizeShip = 3;
 						do
 						{
-							x = rand() % (9 - 0) + 0;
-							y = rand() % (7 - 0) + 0;
-						} while ((cells1[y][x].GetState() == Deck) || (cells1[y][x].GetState() == No));
-						for (int i = 0; i < 3;)
-							if (cells1[y][x + i].GetState() != No)
-								i++;
-							else
-								flag1 = false;
+							do
+							{
+								x = rand() % (7 - 0) + 0;
+								y = rand() % (9 - 0) + 0;
+							} while ((cells1[y][x].GetState() == Deck) || (cells1[y][x].GetState() == No));
+							for (int i = 0; i < 3;)
+								if (cells1[y][x + i].GetState() != No)
+									i++;
+								else
+									flag1 = false;
+						} 
+						while (flag1 == false);
 						if (flag1 == true)
 						{
 							for (int i = 0; i < sizeShip; i++)
-								cells1[y + i][x].SetState(Deck);
+								cells1[y][x + i].SetState(Deck);
 							count1++;
 							size3--;
 							if (y > 0)
@@ -266,10 +275,11 @@ public:
 									if ((y > 0) && (y < 11))
 
 										cells1[y - 1][x - 1].SetState(No);
-								if (x < 6)
+								if (x < 7)
 									if ((y > 0) && (y < 11))
 
 										cells1[y - 1][x + 3].SetState(No);
+								y++;
 							}
 							count++;
 						}
@@ -287,19 +297,18 @@ public:
 					gorizontal = rand() % (1 - 0) + 0;
 					switch (gorizontal)
 					{
-					case 0: // √оризонтальный
+					case 0: // ¬ертикальный (Vert)
 						sizeShip = 2;
 						do {
 							do
 							{
-								x = rand() % (8 - 0) + 0;
-								y = rand() % (9 - 0) + 0;
+								x = rand() % (9 - 0) + 0;
+								y = rand() % (8 - 0) + 0;
 							} while ((cells1[y][x].GetState() == Deck) || (cells1[y][x].GetState() == No));
-							for (int i = 0; i < 3;)
-								if (cells1[y][x + 1].GetState() != No)
-									i++;
-								else
-									flag1 = false;
+							if (cells1[y + 1][x].GetState() != No)
+								flag1 = true;
+							else
+								flag1 = false;
 
 						} while (flag1 == false);
 							for (int i = 0; i < sizeShip; i++)
@@ -326,17 +335,24 @@ public:
 							}
 							count++;
 						break;
-					case 1:// вертикальный
+					case 1:// √оризонтальный (Gor)
 						sizeShip = 2;
 						do
 						{
-							x = rand() % (9 - 0) + 0;
-							y = rand() % (8 - 0) + 0;
-						} while ((cells1[y][x].GetState() == Deck) && (cells1[y][x].GetState() == No) && (cells1[y][x + 1].GetState() != No));
+							do
+							{
+								x = rand() % (8 - 0) + 0;
+								y = rand() % (9 - 0) + 0;
+							} while ((cells1[y][x].GetState() == Deck) || (cells1[y][x].GetState() == No));
+							if (cells1[y][x + 1].GetState() != No)
+								flag1 = true;
+							else
+								flag1 = false;
+						} while (flag1 == false);
 						if (flag1 == true)
 						{
 							for (int i = 0; i < sizeShip; i++)
-								cells1[y + i][x].SetState(Deck);
+								cells1[y][x + i].SetState(Deck);
 							count1++;
 							size2--;
 							if (y > 0)
@@ -349,12 +365,11 @@ public:
 							{
 								if (x > 0)
 									if ((y > 0) && (y < 11))
-
 										cells1[y - 1][x - 1].SetState(No);
-								if (x < 6)
+								if (x < 8)
 									if ((y > 0) && (y < 11))
-
 										cells1[y - 1][x + 2].SetState(No);
+								y++;
 							}
 							count++;
 						}
@@ -378,6 +393,20 @@ public:
 							cells1[y + i][x].SetState(Deck);
 						count1++;
 						size1--;
+						if (y > 0)
+								cells1[y - 1][x].SetState(No);
+						if (y < 9)
+								cells1[y + 1][x].SetState(No);
+						for (int i = 0; i < 3; i++)
+						{
+							if (x > 0)
+								if ((y > 0) && (y < 11))
+									cells1[y - 1][x - 1].SetState(No);
+							if (x < 9)
+								if ((y > 0) && (y < 11))
+									cells1[y - 1][x + 1].SetState(No);
+							y++;
+						}
 					if (size1 == 0)
 						flag = true;
 				} while (flag == false);
@@ -409,32 +438,33 @@ public:
 	}
 	void ShowBoard()
 	{
-		cout << "0123456789" << endl;
+		cout << "       0 1 2 3 4 5 6 7 8 9                  0 1 2 3 4 5 6 7 8 9" << endl;
+		cout << endl;
 		for (int i = 0; i < 10; i++)
 		{
-			cout << i ;
-			for (int j = 0; j < 0; j++)
+			cout << i << "     "<< "|";
+			for (int j = 0; j < 10; j++)
 			{
 				cells[i][j].Print();
+				cout << "|";
+			}
+			cout << "          ";
+			cout << i << "     " << "|";
+			for (int j = 0; j < 10; j++)
+			{
+				cells3[i][j].Print();
+				cout << "|";
 			}
 			cout << endl;
 		}
 	}
-	void Vivod()
-	{
-		for (int i = 0; i < _size; i++) {
-			for (int j = 0; j < _size; j++)
-				cells1[i][j].Print();
-			cout << endl;
-		}
-	}
 };
-void main(int argc, char** argv)
+void main()
 {
 	GameBoard Board;
 	int count = 0, size, x, y;
 	bool hor;
-	/*while (count < 10)
+	while (count < 10)
 	{
 		cout << "vvedite razmer" << endl;
 		cin >> size;
@@ -446,8 +476,21 @@ void main(int argc, char** argv)
 		cin >> hor;
 		Board.CreatorShipPlayer(size,y,x,hor);
 		count++;
-	}*/
+	}
 	Board.CreatorShipIi();
-	Board.Vivod();
+	Board.ShowBoard();
+	do
+	{
+		cout << "Enter x coord: " << endl;
+		int x;
+		cin >> x;
+		cout << "Enter y coord: " << endl;
+		int y;
+		cin >> y;
+		Board.ShootPlayer(y, x);
+		Board.ShootIi();
+		system("cls");
+		Board.ShowBoard();
+	} while (!Board.GameEnd());
 	system("pause");
 }
